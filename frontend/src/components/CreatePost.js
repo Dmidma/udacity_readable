2f11ab4a8a12cddc607c2107f8259acd9aba321a
Dmidma <d.oussema.d@gmail.com>
2018-03-18 18:16:27 +0100

    Added a component to handle creating post form

---
+++ frontend/src/components/CreatePost.js
@@ -0,0 +1,13 @@
+import { Component } from 'react'
+
+import CreatePostTemplate from '../templates/CreatePostTemplate'
+
+class CreatePost extends Component {
+    render() {
+        return (
+            CreatePostTemplate()
+        )
+    }
+}
+
+export default CreatePost
---
+++ frontend/src/templates/CreatePostTemplate.js
@@ -0,0 +1,21 @@
+/**
+ * This is a generated Template for CreatePost Component.
+ * This is used to better organize things.
+ *
+ * You can copy the content of the JSX content into the render 
+ * function of the component.
+ *
+ * It can be safely deleted.
+ */
+
+import React from 'react'
+
+
+const CreatePostTemplate = () => (
+    <div>
+        This is CreatePost component
+    </div>
+)
+
+
+export default CreatePostTemplate
