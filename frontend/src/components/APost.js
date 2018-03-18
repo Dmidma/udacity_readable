31a881fdbd059d4ebca3422884dac3583505dc46
Dmidma <d.oussema.d@gmail.com>
2018-03-18 04:30:59 +0100

    Fetching details of a post asynch.

--- frontend/src/components/APost.js
+++ frontend/src/components/APost.js
@@ -1,9 +1,33 @@
 import { Component } from 'react'
 
-// FIXME: You may need to check this path
 import APostTemplate from '../templates/APostTemplate'
 
+import { getPostById } from '../utils/api'
+
+
+
 class APost extends Component {
+
+    state = {
+        post: {}
+    }
+
+
+    componentDidMount() {
+        
+        getPostById(this.props.match.params.post_id).then(post => {
+            if (post.category !== this.props.match.params.category) {
+                this.props.history.replace("/pagenotfound")
+            } else {
+                this.setState({ post })
+            }
+
+        })
+    }
+
+
+
+
     render() {
         return (
             APostTemplate(this.props.match.params.category, this.props.match.params.post_id)
