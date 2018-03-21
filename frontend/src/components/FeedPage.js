a297a48b08e7da5bf32abbb134ed7c8bef5e2600
Dmidma <d.oussema.d@gmail.com>
2018-03-21 07:41:05 +0100

    Added box to specify the sort method

--- frontend/src/components/FeedPage.js
+++ frontend/src/components/FeedPage.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import HeaderActionBar from './HeaderActionBar'
 import PostCard from './PostCard'
+import SelectField from 'material-ui/SelectField'
+import MenuItem from 'material-ui/MenuItem'
 
 import { getPosts } from '../utils/api'
 
@@ -12,21 +14,44 @@ import { addCategories } from '../actions/categoriesActions'
 
 
 class FeedPage extends Component {
+
+    sortArray = ["best", "worst", "new", "old"]
+    state = {
+        sort: 1
+    }
     
     componentDidMount() {
-        
-        const sort = this.props.match.params.sort
-        if (sort !== undefined && sort !== "best")
-            this.props.setPostSorting(sort)
-
         if (this.props.username === null) {
             this.props.history.push("/")
         } else {
+            const sort = this.props.match.params.sort
+            this.setState({ sort: this.mapSortToValue(sort) })
+            if (sort !== undefined && sort !== "best")
+            this.props.setPostSorting(sort)
             getPosts().then(data => {
                 this.props.fetchPosts(data)
             }) 
             this.props.fetchCategories()
         }
+    }
+    
+    mapSortToValue = (sort) => {
+        switch (sort) {
+            case "worst":
+                return 2
+            case "new":
+                return 3
+            case "old":
+                return 4
+            default:
+                return 1
+        }
+    }
+
+    handleSortChanging = (e, i, v) => {
+        this.props.history.push(`/feed/${this.sortArray[v - 1]}`)
+        // to rerender the whole page
+        window.location.reload()
     }
 
 
@@ -35,17 +60,18 @@ class FeedPage extends Component {
         return (
             <div>
                 <HeaderActionBar />
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                    { this.props.postsIds.map(id => (<PostCard key={id} postId={id} />)) }
+                <SelectField
+                  className="sort-box"
+                  floatingLabelText="Sort by:"
+                  value={this.state.sort}
+                  onChange={this.handleSortChanging}
+                >
+                  <MenuItem value={1} primaryText="best" />
+                  <MenuItem value={2} primaryText="worst" />
+                  <MenuItem value={3} primaryText="new" />
+                  <MenuItem value={4} primaryText="old" />
+                </SelectField>
+                { this.props.postsIds.map(id => (<PostCard key={id} postId={id} />)) }
             </div>
         )
     }
