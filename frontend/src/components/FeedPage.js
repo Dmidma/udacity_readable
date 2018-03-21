5431924a23b03885a7f737966ea3cdc64040c2d0
Dmidma <d.oussema.d@gmail.com>
2018-03-21 10:54:06 +0100

    Added functionality of pagination with query String from url

--- frontend/src/components/FeedPage.js
+++ frontend/src/components/FeedPage.js
@@ -10,18 +10,28 @@ import { getPosts } from '../utils/api'
 import { addPosts, setSort } from '../actions/postsActions'
 
 import { addCategories } from '../actions/categoriesActions'
-
+import { parse } from 'qs'
 
 class FeedPage extends Component {
 
     state = {
-        sort: "best"
+        sort: "best",
+        page: 1
     }
     
     componentDidMount() {
         if (this.props.username === null) {
             this.props.history.push("/")
         } else {
+
+            const queryString = this.props.history.location.search
+            if (queryString) {
+                const queryObj = parse(queryString, { ignoreQueryPrefix: true })
+                if (queryObj.page) {
+                    this.setState({ page: queryObj.page })
+                }
+            }
+
             let sort = this.props.match.params.sort
             if (sort === undefined) sort = "best"
             if (sort !== "best") {
@@ -34,15 +44,23 @@ class FeedPage extends Component {
             this.props.fetchCategories()
         }
     }
+
+
+    getPostsForPage = (page) => {
+        const currentPage = page - 1
+        const currentIndex = currentPage * 3
+        const subPosts = this.props.postsIds.slice(currentIndex, currentIndex + 3)
+
+        return subPosts.map(id => (<PostCard key={id} postId={id} />))
+    }
     
     render() {
         if (this.props.username === null) return null
-        console.log(this.state.sort)
         return (
             <div>
                 <HeaderActionBar  />
                 <SortBox sort={this.state.sort} />
-                { this.props.postsIds.map(id => (<PostCard key={id} postId={id} />)) }
+                { this.getPostsForPage(this.state.page) }
             </div>
         )
     }
