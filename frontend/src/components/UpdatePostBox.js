cf85f7615f51d229e0afd49b65db2a4556945333
Dmidma <d.oussema.d@gmail.com>
2018-03-21 23:58:37 +0100

    Added confirm messages for edit/delete

--- frontend/src/components/UpdatePostBox.js
+++ frontend/src/components/UpdatePostBox.js
@@ -3,27 +3,57 @@ import { Component } from 'react'
 import UpdatePostBoxTemplate from '../templates/UpdatePostBoxTemplate'
 import PropTypes from 'prop-types'
 
+
+// import { withRouter } from 'react-router-dom'
+import { connect } from 'react-redux'
+// import { actionCreator } from '../actions'
+
 class UpdatePostBox extends Component {
 
     static propTypes = {
-        postId: PropTypes.string.isRequired
+        postId: PropTypes.string.isRequired,
+        postTitle: PropTypes.string.isRequired
     }
     
-    
+    state = {
+        isConfirmOpen: false,
+        confirmMessage: ''
+    }
+
+    closeConfirmDialog = () => this.setState({ isConfirmOpen: false })
+    openConfirmDialog = () => this.setState({ isConfirmOpen: true })
 
     handleEdit = () => {
-        console.log("click edit")
+        this.setConfirmMessage("edit")
+        this.openConfirmDialog()
     }
 
     handleDelete = () => {
-        console.log("click delete")
+        this.setConfirmMessage("delete")
+        this.openConfirmDialog()
     }
+
+    setConfirmMessage = (action) => this.setState({ confirmMessage: `Are you sure you to ${action} post: "${this.props.postTitle}"?` }) 
 
     render() {
         return (
-            UpdatePostBoxTemplate(this.handleEdit, this.handleDelete)
+            UpdatePostBoxTemplate(this.handleEdit, this.handleDelete, this.state.confirmMessage, this.state.isConfirmOpen, this.closeConfirmDialog)
         )
     }
 }
 
-export default UpdatePostBox
+function mapStateToProps ({ posts }, ownProps) {
+    return {
+        postTitle: posts[ownProps.postId].title
+    }
+}
+
+function mapDispatchToProps (dispatch) {
+    return {
+        // propsName: () => dispatch(actionCreator())
+    }
+}
+
+
+// export default withRouter(connect(mapStateToProps, mapDispatchToProps)(UpdatePostBox))
+export default connect(mapStateToProps, mapDispatchToProps)(UpdatePostBox)
--- frontend/src/templates/UpdatePostBoxTemplate.js
+++ frontend/src/templates/UpdatePostBoxTemplate.js
@@ -1,27 +1,53 @@
 import React from 'react'
 import FontIcon from 'material-ui/FontIcon'
+import Dialog from 'material-ui/Dialog'
 import { blue500 } from 'material-ui/styles/colors'
+import FlatButton from 'material-ui/FlatButton'
 
+const UpdatePostBoxTemplate = (handleEdit, handleDelete, confirmMessage, isConfirmOpen, closeConfirmDialog) => {
+    const actions = [
+      <FlatButton
+        label="No"
+        primary={true}
+        onClick={closeConfirmDialog}
+      />,
+      <FlatButton
+        label="Yes"
+        primary={true}
+        keyboardFocused={true}
+        onClick={closeConfirmDialog}
+      />,
+    ];
+    return (
+        <div className="update-section">
+            <FontIcon
+                className="material-icons"
+                hoverColor={blue500} 
+                onClick={handleEdit}
+            >
+                    mode_edit
+            </FontIcon>
 
-const UpdatePostBoxTemplate = (handleEdit, handleDelete) => (
-    <div className="update-section">
-        <FontIcon
-            className="material-icons"
-            hoverColor={blue500} 
-            onClick={handleEdit}
-        >
-                mode_edit
-        </FontIcon>
+            <FontIcon
+                className="material-icons"
+                hoverColor={blue500} 
+                onClick={handleDelete}
+            >
+                    delete
+            </FontIcon>
 
-        <FontIcon
-            className="material-icons"
-            hoverColor={blue500} 
-            onClick={handleDelete}
-        >
-                delete
-        </FontIcon>
-    </div>
-)
+            <Dialog
+              title="Are you Sure?"
+              modal={false}
+              actions={actions}
+              open={isConfirmOpen}
+              onRequestClose={closeConfirmDialog}
+            >
+                {confirmMessage}
+            </Dialog>
+        </div>
+    )
+}
 
 
 export default UpdatePostBoxTemplate
