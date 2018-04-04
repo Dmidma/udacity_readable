fa684e21d5b2922f788b7b921937aa8db2bbefdf
Dmidma <d.oussema.d@gmail.com>
2018-04-04 22:59:46 +0100

    Refactored CommentCard

--- frontend/src/components/CommentCard/CommentCard.js
+++ frontend/src/components/CommentCard/CommentCard.js
@@ -1,39 +1,29 @@
-import { Component } from 'react'
-
 import CommentCardTemplate from './CommentCardTemplate'
 import PropTypes from 'prop-types'
 
-class CommentCard extends Component {
+const CommentCard = ({
+    upVoteComment,
+    downVoteComment,
+    editComment,
+    deleteComment,
+    comment,
+    isOfLoggedUser
+}) => (
+    CommentCardTemplate(
+        comment,
+        upVoteComment, downVoteComment,
+        editComment, deleteComment,
+        isOfLoggedUser
+    )
+)
 
-    static propTypes = {
-        comment: PropTypes.object.isRequired,
-        upVoteComment: PropTypes.func.isRequired,
-        downVoteComment: PropTypes.func.isRequired,
-        editComment: PropTypes.func.isRequired,
-        deleteComment: PropTypes.func.isRequired,
-        isOfLoggedUser: PropTypes.bool.isRequired
-    }
-
-
-
-    render() {
-        const { 
-            upVoteComment,
-            downVoteComment, 
-            editComment, 
-            deleteComment,
-            comment,
-            isOfLoggedUser
-        } = this.props
-        return (
-            CommentCardTemplate(
-                comment,
-                upVoteComment, downVoteComment,
-                editComment, deleteComment,
-                isOfLoggedUser
-            )
-        )
-    }
+CommentCard.propTypes = {
+    comment: PropTypes.object.isRequired,
+    upVoteComment: PropTypes.func.isRequired,
+    downVoteComment: PropTypes.func.isRequired,
+    editComment: PropTypes.func.isRequired,
+    deleteComment: PropTypes.func.isRequired,
+    isOfLoggedUser: PropTypes.bool.isRequired
 }
 
 export default CommentCard
